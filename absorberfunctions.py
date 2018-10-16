986d644785b7dffa612d3ea20c95b6addad3c114
Treeed <freddyallesweg@gmail.com>
2018-10-16 17:20:23 +0200

    fixed import image, make handle, reset handles after file split

--- absorberfunctions.py
+++ absorberfunctions.py
@@ -1,37 +1,37 @@
 import math
-
-def add_bs(self):
-    new_list = []
-    buf_list = self.roiPos
-    bs = [10, 10]
-    target = []
-    for i in range(0, len(buf_list)):
-        buf_list[i][2] = self.calc_vec_len(buf_list[i][0], bs[0], buf_list[i][1], bs[1])
-    target.append(buf_list[len(buf_list) - 1])
-    target[0].append(0)
-    for i in range(0, len(buf_list)):
-        if buf_list[i][2] < target[0][2]:
-            new_list.append(buf_list[i])
-    target[0][3] = self.calc_alpha(target[0][0], bs[0], target[0][1], bs[1])
-    for i in range(0, len(new_list)):
-        new_list[i].append(0)
-        new_list[i][3] = new_list[0][2] * math.tan(
-            math.pi / 180 * self.calc_alpha(new_list[i][0], bs[0], new_list[i][1], bs[1]) - target[0][3])
+import pyqtgraph as pg
 
 
-def reset_roi(self):
-    for x in range(0, len(self.roiAll)):
-        self.imv.removeItem(self.roiAll[x])
-    self.roiAll = []
-    self.roiPos = []
+class BeamstopManager:
+    def __init__(self, im_view, absorber_hardware):
+        self.im_view = im_view
+        self.absorber_hardware = absorber_hardware
 
-def update(self):
-    for x in range(0, len(self.roiAll)):
-        self.roiPos[x] = [self.roiAll[x].pos()[0], self.roiAll[x].pos()[1], 0]
-        self.roiSize[x] = self.roiAll[x].size()
-def change(self):
-    self.roiAll.append(pg.CircleROI([5, 5, 0], [20, 20], pen=(9, 15)))
-    self.roiAll[len(self.roiAll) - 1].sigRegionChanged.connect(self.update)
-    self.imv.addItem(self.roiAll[len(self.roiAll) - 1])
-    self.roiPos.append([5, 5, 0])
-    self.roiSize.append([20, 20])
+        self.beamstop_handles = [] #movable handles to specify the targets where the beamstops will be moved
+
+    def reset_all(self):
+        for beamstop in self.beamstop_handles:
+            self.im_view.removeItem(beamstop)
+        #TODO: reset the physical beamstops to their original positions
+
+    def add_handle(self):
+        self.beamstop_handles.append(pg.CircleROI([5, 5], [20, 20], pen=(9, 15)))
+        self.im_view.addItem(self.beamstop_handles[-1])
+
+    def calcnextbs(self):
+        new_list = []
+        buf_list = self.roiPos
+        bs = [10, 10]
+        target = []
+        for i in range(0, len(buf_list)):
+            buf_list[i][2] = self.calc_vec_len(buf_list[i][0], bs[0], buf_list[i][1], bs[1])
+        target.append(buf_list[len(buf_list) - 1])
+        target[0].append(0)
+        for i in range(0, len(buf_list)):
+            if buf_list[i][2] < target[0][2]:
+                new_list.append(buf_list[i])
+        target[0][3] = self.calc_alpha(target[0][0], bs[0], target[0][1], bs[1])
+        for i in range(0, len(new_list)):
+            new_list[i].append(0)
+            new_list[i][3] = new_list[0][2] * math.tan(
+                math.pi / 180 * self.calc_alpha(new_list[i][0], bs[0], new_list[i][1], bs[1]) - target[0][3])
--- absorbergui.py
+++ absorbergui.py
@@ -15,9 +15,10 @@ class ViewData(QtGui.QMainWindow):
 
         self.file_handler = fileio.FileHandler(self.im_view, self.widget)
         self.absorber_hardware = hardware.PeakAbsorberHardware()
+        self.beamstops = absorberfunctions.BeamstopManager(self.im_view)
 
-        button_new_target = QtGui.QPushButton("new target")
-        button_new_target.clicked.connect(absorberfunctions.change)
+        button_new_target = QtGui.QPushButton("new beamstop")
+        button_new_target.clicked.connect(self.beamstops.add_handle)
 
         button_open_file = QtGui.QPushButton("open image")
         button_open_file.clicked.connect(self.file_handler.open_file)
@@ -25,8 +26,8 @@ class ViewData(QtGui.QMainWindow):
         button_write_to_file = QtGui.QPushButton("write to file")
         button_write_to_file.clicked.connect(self.file_handler.write_data)
 
-        button_reset_roi = QtGui.QPushButton("reset ROI")
-        button_reset_roi.clicked.connect(absorberfunctions.reset_roi)
+        button_reset_all_beamstops = QtGui.QPushButton("reset all beamstops")
+        button_reset_all_beamstops.clicked.connect(self.beamstops.reset_all)
 
         button_read_file = QtGui.QPushButton("read file")
         button_read_file.clicked.connect(self.file_handler.read_data)
@@ -40,20 +41,16 @@ class ViewData(QtGui.QMainWindow):
         button_re_arrange = QtGui.QPushButton("rearrange")
         button_re_arrange.clicked.connect(self.absorber_hardware.rearrange)
 
-        button_add_bs = QtGui.QPushButton("add_bs")
-        button_add_bs.clicked.connect(absorberfunctions.add_bs)
-
 
         self.widget.layout().addWidget(self.im_view, 0, 0, 3, 3)
         self.widget.layout().addWidget(button_new_target, 4, 0)
         self.widget.layout().addWidget(button_open_file, 4, 1)
         self.widget.layout().addWidget(button_write_to_file, 4, 2)
-        self.widget.layout().addWidget(button_reset_roi, 5, 0)
+        self.widget.layout().addWidget(button_reset_all_beamstops, 5, 0)
         self.widget.layout().addWidget(button_move_pellets, 5, 1)
         self.widget.layout().addWidget(button_read_file, 5, 2)
         self.widget.layout().addWidget(button_calibrate, 6, 0)
         self.widget.layout().addWidget(button_re_arrange, 6, 1)
-        self.widget.layout().addWidget(button_add_bs, 6, 2)
         self.setCentralWidget(self.widget)
         self.show()
 
