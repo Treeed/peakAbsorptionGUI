272b07080b2afc7e089521a5b02a8857063b2182
mainberg <freddyallesweg@gmail.com>
2018-11-08 16:23:56 +0100

    changed the layout to use the space on a widescreen display better and switched to a dark theme to not kill the eyes with the contrast

--- peakAbsorptionGUI.py
+++ peakAbsorptionGUI.py
@@ -1,10 +1,13 @@
 from PyQt5 import QtGui
 import sys
 import absorbergui
+import qdarkstyle
 
 
 def main():
     app = QtGui.QApplication(sys.argv)
+    # This has a deprecation warning but doesn't provide the function replacing the deprecated one yet. Seems we have to live with it.
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     vd = absorbergui.MainWindow()
     vd.show()
     app.exec_()
